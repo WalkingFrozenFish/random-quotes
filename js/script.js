910673296c42d2c93ce5bf4bb858fe67f5e2a624
Oleg Chachin <44916575+WalkingFrozenFish@users.noreply.github.com>
2021-05-16 14:14:47 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+let url = "https://favqs.com/api/qotd";
+let text = document.querySelector(".text-block");
+let name = document.querySelector(".name-block");
+let button = document.querySelector(".generate-quote");
+
+
+button.addEventListener("click", function() {
+    fetch(url)
+    .then(function(data) {
+        console.log(data)
+        return data.json();
+    })
+    .then(function(data2) {
+        console.log(data2);
+        text.innerHTML = `"${data2.quote.body}"`;
+        name.innerHTML = data2.quote.author;
+    })
+});
